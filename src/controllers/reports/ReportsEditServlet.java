2fb1411d0a775cd7cdc1bf56148b2b2d70a4cd5f
saki-fkd <fukusuke4869@gmail.com>
2020-08-11 22:32:25 +0900

    Add Report show

--- src/controllers/reports/ReportsEditServlet.java
+++ src/controllers/reports/ReportsEditServlet.java
@@ -2,11 +2,15 @@ package controllers.reports;
 
 import java.io.IOException;
 
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Report;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class ReportsEditServlet
@@ -27,8 +31,13 @@ public class ReportsEditServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+       EntityManager em = DBUtil.createEntityManager();
+       
+       Report r = em.find(Report.class, Integer.parseInt(request.getParameter("id")));
+       
+       em.close();
+       
+       
     }
 
 }
